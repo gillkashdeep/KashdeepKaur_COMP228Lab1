9e445defe10208c4255000ee55f88e475e73bdc3
gillkashdeep <gillkashdeep@gmail.com>
2018-05-19 23:01:06 -0400

    Done with exercise#2

---
+++ src/GameObject.java
@@ -0,0 +1,45 @@
+public class GameObject {
+
+    int center;
+    String velocity;
+    String state;
+    String rotation;
+
+    public GameObject(String velocity, String state, String rotation) {
+        this.velocity = velocity;
+        this.state = state;
+        this.rotation = rotation;
+    }
+
+    public int getCenter() {
+        return center;
+    }
+
+    public void setCenter(int center) {
+        this.center = center;
+    }
+
+    public String getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(String velocity) {
+        this.velocity = velocity;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(String rotation) {
+        this.rotation = rotation;
+    }
+}
